b900352e29033fc4c70cfa7b1d21ee0d7cb78200
Robin Trujillo <109051229+robtrucon@users.noreply.github.com>
2023-09-11 23:50:15 -0600

    Add files via upload

---
+++ TipoDatoVoid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Esta función no devuelve ningún valor (void)
+void saludar() {
+    printf("Hola, mundo!\n");
+}
+
+int main() {
+    // Llamamos a la función saludar
+    saludar();
+
+    return 0;
+}
