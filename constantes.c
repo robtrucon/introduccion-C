72c7a5c9f942155d305783a22b31199b588a4265
Robin Trujillo <109051229+robtrucon@users.noreply.github.com>
2023-09-12 00:53:41 -0600

    Add files via upload

---
+++ constantes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Definir una constante llamada PI
+#define PI 3.14159265359
+
+int main() {
+    // Calcular el área de un círculo utilizando la constante PI
+    double radio = 5.0;
+    double area = PI * radio * radio;
+
+    // Imprimir el resultado
+    printf("El área del círculo es: %f\n", area);
+
+    return 0;
+}
