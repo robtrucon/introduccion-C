fafa6c9b08753ce731a97b3e8d41cb2a3d225cb6
Robin Trujillo <109051229+robtrucon@users.noreply.github.com>
2023-10-12 23:00:49 -0600

    Update and rename enums.c to 4_tipoDatoEnum
    Programa que ejemplifica el uso del tipo de dato enum.

--- 4_tipoDatoEnum.c
+++ 4_tipoDatoEnum.c
@@ -1,8 +1,10 @@
 //Librería para entrada y salidas e imprimir en pantalla
 #include <stdio.h>
 
-// Definición del enumerador "weekDays" para los días de la semana, asociandolo con un valor entero del 0 al 6
+// Definición del enumerador "weekDays" para los días de la semana, asociandolo con un valor entero del 0 al 6.
+// en base a su posicion.
 enum weekDays {
+               //Posiciones de los dias de la semana
     Monday,    // 0
     Tuesday,   // 1
     Wednesday, // 2
@@ -14,9 +16,9 @@ enum weekDays {
 
 int main() {
     enum weekDays today; // Declaración de una variable llamada 'today' del tipo 'enum weekDays'
-    today = Monday;     // Asignación del valor 'Sunday' (6) a la variable 'today'
+    today = Monday;     // Asignación del valor 'Monday' (0) a la variable 'today'
 
-    printf("Day %d", today + 1); // Se imprime "Day" seguido del valor de today + 1. 
+    printf("Day %d", today + 1); // Se imprime "Day" seguido del valor de today + 1. (se le suma 1 porque empieza de 0)
     
     return 0; // Retorna un valor (0) para indicar que el programa se ejecutó correctamente
 }
